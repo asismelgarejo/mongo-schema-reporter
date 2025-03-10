813a0921298828337492ddb51e997a29fa4a0207
asismelgarejo <asismelgarejo@gmail.com>
2025-03-10 15:05:09 -0500

    refactor: notation for objects and arrays

--- src/fields/index.ts
+++ src/fields/index.ts
@@ -104,7 +104,7 @@ class Engine {
     const fieldsHashMap: FieldsHashMap = {};
     props.schemas.forEach((schema, idx) => {
       const discriminator = schema.title ?? this.numberToLetters(idx);
-      const pathField = `${props.path}[]${discriminator}`;
+      const pathField = `${props.path}[${discriminator}]`;
       const r = this.processObject(schema, pathField);
       Object.assign(fieldsHashMap, r.fieldsHashMap);
     });
